5c6eaa1c7f689d5e2e04049bbf49afb49ffb51c7
hgode <hjgode@gmail.com>
2021-11-06 09:05:19 +0100

    About to add Itext

--- app/src/main/java/com/dev2qa/example/layout/table/TableLayoutActivity.java
+++ app/src/main/java/com/dev2qa/example/layout/table/TableLayoutActivity.java
@@ -1,10 +1,22 @@
 //TableLayoutActivity
 package com.dev2qa.example.layout.table;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +28,14 @@ import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.gcacace.signaturepad.views.SignaturePad;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +43,14 @@ public class TableLayoutActivity extends AppCompatActivity implements View.OnKey
 
     private Context context = null;
     int currentRowNumber=0;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        verifyStoragePermissions(this);
+
         setContentView(R.layout.activity_table_layout);
 
         setTitle("Artikel Liste");
@@ -113,7 +136,55 @@ public class TableLayoutActivity extends AppCompatActivity implements View.OnKey
             }
         });
 
-    }
+        SignaturePad mSignaturePad=(SignaturePad)findViewById(R.id.signature_pad);
+
+        Button buttonSaveSignature=(Button)findViewById(R.id.saveSignatureButton);
+
+        buttonSaveSignature.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   Bitmap signatureBitmap=mSignaturePad.getSignatureBitmap();
+                   //save the bitmap
+                   if (addJpgSignatureToGallery(signatureBitmap)) {
+                       Toast.makeText(context, "Signature saved into the Gallery", Toast.LENGTH_SHORT).show();
+                       mSignaturePad.setEnabled(false);
+                   } else {
+                       Toast.makeText(context, "Unable to store the signature", Toast.LENGTH_SHORT).show();
+                   }
+
+               }
+           });
+        Button buttonClearSignature = (Button) findViewById(R.id.clearSignatureButton);
+        buttonClearSignature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignaturePad.clear();
+                mSignaturePad.setEnabled(true);
+            }
+        });
+        Button buttonCreatePDF=(Button)findViewById(R.id.button_create_pdf);
+        mSignaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+                //Toast.makeText(MainActivity.this, "OnStartSigning", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSigned() {
+                buttonSaveSignature.setEnabled(true);
+                buttonClearSignature.setEnabled(true);
+                buttonCreatePDF.setEnabled(true);
+            }
+
+            @Override
+            public void onClear() {
+                buttonClearSignature.setEnabled(false);
+                buttonSaveSignature.setEnabled(false);
+                buttonCreatePDF.setEnabled(true);
+            }
+        });
+
+    }//onCreate
 
     @Override
     public boolean onKey(View view, int keyCode, KeyEvent event) {
@@ -128,5 +199,76 @@ public class TableLayoutActivity extends AppCompatActivity implements View.OnKey
             }
         }
         return false;
+    }
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p/>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity the activity from which permissions are checked
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(TableLayoutActivity.this, "Cannot write images to external storage", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e("SignaturePad", "Directory not created");
+        }
+        return file;
+    }
+    public boolean addJpgSignatureToGallery(Bitmap signature) {
+        boolean result = false;
+        try {
+            File photo = new File(getAlbumStorageDir("SignaturePad"), "Signature_lieferschein.jpg");
+            saveBitmapToJPG(signature, photo);
+            scanMediaFile(photo);
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    private void scanMediaFile(File photo) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(photo);
+        mediaScanIntent.setData(contentUri);
+        TableLayoutActivity.this.sendBroadcast(mediaScanIntent);
+    }
+
+    public void saveBitmapToJPG(Bitmap bitmap, File photo) throws IOException {
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        OutputStream stream = new FileOutputStream(photo);
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+        stream.close();
     }
 }
